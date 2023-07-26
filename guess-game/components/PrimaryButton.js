2630c0b45cd4ca4135e5a8c17aafe5091bb173db
s.jafari <saeid8.jafari@gmail.com>
2023-07-26 15:51:58 +0330

    add presable

--- guess-game/components/PrimaryButton.js
+++ guess-game/components/PrimaryButton.js
@@ -1,27 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import React from "react";
 
-export default function PrimaryButton({children}) {
+export default function PrimaryButton({ children, onPress }) {
   return (
-    <View style={styles.buttonContainer}>
-      <Text style={styles.buttonTextColor}>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={(pressed) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={onPress}
+        android_ripple={{ color: "#4527a0" }}
+      >
+        <Text style={styles.buttonTextColor}>{children}</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer:{
-        width:80,
-        height:50,
-        backgroundColor:"#7e57c2",
-        display:'flex',
-        justifyContent:"center",
-        alignItems:'center',
-        borderRadius:16,
-       
-    },
+  buttonOuterContainer: {
+    borderRadius: 8,
+    marginHorizontal: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    width: 70,
+    height: 40,
+    backgroundColor: "#7e57c2",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    marginHorizontal: 4,
+    elevation: 4,
+  },
 
-    buttonTextColor:{
-        color:'white'
-    }
-})
+  buttonTextColor: {
+    color: "white",
+    textAlign: "center",
+  },
+
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- guess-game/screens/StartGameScreen.js
+++ guess-game/screens/StartGameScreen.js
@@ -1,22 +1,34 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput  } from "react-native";
 import React from "react";
 import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return (
     <View style={styles.container}>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.inputNumber}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        maxLength={2}
+      />
+      <View style={styles.buttonContainer}>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+        
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     width: "100%",
     height: 200,
     padding: 16,
-    borderRadius:16,
+    borderRadius: 16,
     backgroundColor: "#b39ddb",
     elevation: 4,
     shadowColor: "black",
@@ -24,5 +36,21 @@ const styles = StyleSheet.create({
     shadowRadius: 6,
     shadowOpacity: 0.25,
     marginTop: 100,
+  },
+
+  inputNumber: {
+    borderColor: "#ffeb3b",
+    borderWidth: 2,
+    borderRadius: 8,
+    marginVertical: 8,
+    width: 50,
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fbc02d",
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
   },
 });
