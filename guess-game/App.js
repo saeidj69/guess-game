a0cf475c4fc0c6bef9f95c5a853aa9c155fbe6cf
s.jafari <saeid8.jafari@gmail.com>
2023-07-29 16:20:21 +0330

    add expo cli and gradiaant and background

--- guess-game/App.js
+++ guess-game/App.js
@@ -1,16 +1,29 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
+
 export default function App() {
   return (
-    <View style={styles.conatiner}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient colors={["#b39ddb", "#ffeb3b"]} style={styles.conatiner}>
+      <ImageBackground
+        source={require("./assets/images/backgroundImage.jpg")}
+        resizeMode="cover"
+        style={styles.conatiner}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   conatiner: {
     flex: 1,
-    padding: 16,
+    
+  },
+
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
